f7e074ed9630ff2caf14ab9cbb4c941de7a791c8
TANISHQ TIWARI <tanishq.tiwari2812@gmail.com>
2025-05-30 18:50:55 +0530

    Initial commit with project files

---
+++ app.js
@@ -0,0 +1,30 @@
+const { log } = require("console");
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path"); 
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); 
+
+io.on("connection" , function(socket){
+    socket.on("send-location" , function(data) {
+        io.emit("recieve-location" , {id: socket.id , ...data });
+    });
+
+    socket.on("disconnect" , function() {
+        io.emit("user-disconnected , socket.id");
+    }); 
+    
+    //Debugging check
+    console.log("connected");
+})
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(3000);
